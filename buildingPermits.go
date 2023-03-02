da3307319940f6c688ca587809c0638587865f69
andrewgray11 <71670031+andrewgray11@users.noreply.github.com>
2023-03-01 21:18:04 -0800

    Update buildingPermits.go

--- buildingPermits.go
+++ buildingPermits.go
@@ -5,35 +5,44 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 
 	_ "github.com/lib/pq"
 )
 
+const (
+	url    = "https://data.cityofchicago.org/resource/ydr8-5enu.json?$limit=500"
+	dbHost = "localhost"
+	dbPort = 5432
+	dbUser = "postgres"
+	dbPass = "postgres"
+	dbName = "testdb"
+)
+
 type buildingPermit struct {
-	ID                  int    `json:"id"`
-	PermitNumber        string `json:"permit_"` // use backtick to handle column name with underscore
-	PermitType          string `json:"permit_type"`
-	ReviewType			string `json:"review_type"`
-	TotalFee			float64`json:"total_fee"`
-	AppStartDate		string `json:"application_start_date"`
-	IssueDate			string `json:"issue_date"`
-	CommunityArea		int	   `json:"community_area"`
-	Latitude			float64`json:"latitude"`
-	Longitude			float64`json:"longitude"`
+	ID            int     `json:"id"`
+	PermitNumber  string  `json:"permit_"` // use backtick to handle column name with underscore
+	PermitType    string  `json:"permit_type"`
+	ReviewType    string  `json:"review_type"`
+	TotalFee      float64 `json:"total_fee"`
+	AppStartDate  string  `json:"application_start_date"`
+	IssueDate     string  `json:"issue_date"`
+	CommunityArea int     `json:"community_area"`
+	Latitude      float64 `json:"latitude"`
+	Longitude     float64 `json:"longitude"`
 }
 
 func main() {
 	// Set up database connection
-	db, err := sql.Open("postgres", "postgres://user:password@host/database?sslmode=disable")
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		fmt.Println("Error connecting to database:", err)
-		os.Exit(1)
+		panic(err)
 	}
 	defer db.Close()
 
 	// Make API request to City of Chicago Building Permits dataset
-	resp, err := http.Get("https://data.cityofchicago.org/resource/ydr8-5enu.json?$limit=500")
+	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error making API request:", err)
 		return
@@ -52,7 +61,7 @@ func main() {
 	for _, permit := range buildingPermits {
 		query := `INSERT INTO building_permits (id, permit_number, permit_type, review_type, total_fee, application_start_date, issue_date, community_area, latitude, longitude)
 				  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	
+
 		_, err := db.Exec(query, permit.ID, permit.PermitNumber, permit.PermitType, permit.ReviewType, permit.TotalFee, permit.AppStartDate, permit.IssueDate, permit.CommunityArea, permit.Latitude, permit.Longitude)
 		if err != nil {
 			panic(err)
