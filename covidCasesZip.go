7d76f3fde43916f5df37ec04bc436a68dd46a66a
andrewgray11 <71670031+andrewgray11@users.noreply.github.com>
2023-03-03 19:00:28 -0800

    Update covidCasesZip.go
    matched creds and table/column names

--- covidCasesZip.go
+++ covidCasesZip.go
@@ -15,8 +15,8 @@ const (
 	dbHost = "localhost"
 	dbPort = 5432
 	dbUser = "postgres"
-	dbPass = "postgres"
-	dbName = "testdb"
+	dbPass = "root"
+	dbName = "master"
 )
 type CovidZip struct {
 	Zip          string  `json:"zip_code"`
@@ -74,29 +74,29 @@ func main() {
 	// Insert data into database
 	ffor _, row := range rows {
 		_, err := db.Exec(`
-			INSERT INTO covid_zip (
-				zip_code, 
-				week_number, 
-				week_start, 
-				week_end, 
-				cases_weekly, 
-				cases_monthly, 
-				cases_cumulative, 
-				case_rate_weekly, 
-				case_rate_monthly, 
-				case_rate_cumulative, 
-				tests_weekly, 
-				tests_cumulative, 
-				test_rate_weekly, 
-				test_rate_cumulative, 
-				percent_tested_positive_weekly, 
-				percent_tested_positive_cumulative, 
-				deaths_weekly, 
-				deaths_cumulative, 
-				death_rate_weekly, 
-				death_rate_cumulative, 
-				population, 
-				row_id
+			INSERT INTO covidCasesZip (
+				Zip, 
+				WeekNumber, 
+				WeekStart, 
+				WeekEnd, 
+				CasesWeekly, 
+				CasesMonthly, 
+				CasesCum, 
+				CaseRateWkly, 
+				CaseRateMnth, 
+				CaseRateCum, 
+				TestsWeekly, 
+				TestsCum, 
+				TestRateWkly, 
+				TestRateCum, 
+				PosRateWkly, 
+				PosRateCum, 
+				DeathsWkly, 
+				DeathsCum, 
+				DeathRtWkly, 
+				DeathRtCum, 
+				Population, 
+				RowId
 			) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)
 		`, row.Zip, row.WeekNumber, row.WeekStart, row.WeekEnd, row.CasesWeekly, row.CasesMonthly, row.CasesCum, row.CaseRateWkly, row.CaseRateMnth, row.CaseRateCum, row.TestsWeekly, row.TestsCum, row.TestRateWkly, row.TestRateCum, row.PosRateWkly, row.PosRateCum, row.DeathsWkly, row.DeathsCum, row.DeathRtWkly, row.DeathRtCum, row.Population, row.RowId)
 		if err != nil {
