52ed25825e0db077d25f6fe79b1831b59bf757cd
andrewgray11 <71670031+andrewgray11@users.noreply.github.com>
2023-03-03 21:30:38 -0800

    Update create_tables.sql
    updated columns and data types to match go scripts

--- create_tables.sql
+++ create_tables.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS ccvi;
 DROP TABLE IF EXISTS censusData;
 DROP TABLE IF EXISTS communityBound;
 DROP TABLE IF EXISTS covidCasesZip;
-DROP TABLE IF EXISTS covidDaily;
+/*DROP TABLE IF EXISTS covidDaily;*/
 DROP TABLE IF EXISTS pubHealthStats;
 DROP TABLE IF EXISTS taxiTrips;
 DROP TABLE IF EXISTS transNetProviderTrips;
@@ -15,49 +15,46 @@ DROP TABLE IF EXISTS transNetProviderTrips;
 
 CREATE TABLE buildingPermit (
 
-    ID                  INT,    
+    	ID                  INT,    
 	PermitNumber        VARCHAR(100),
 	PermitType          VARCHAR(100),
-	PermitSubType       VARCHAR(100),
-	PermitDescription   VARCHAR(100),
-	PermitIssueDate     VARCHAR(100),
-	PermitEstimatedCost INT,
-	PermitStatus        VARCHAR(100),
-	PermitStreetNumber  INT,
-	PermitStreetName    VARCHAR(100),
-	PermitSuffix        VARCHAR(100),
-	PermitWorkType      VARCHAR(100),
-	PermitPIN1          INT,
-	PermitPIN2          INT
+	ReviewType          VARCHAR(100),
+	TotalFee	    FLOAT,
+	AppStartDate        VARCHAR(100),
+	IssueDate	    VARCHAR(100),
+	CommunityArea       INT,
+	Latitude	    FLOAT,
+	Longitude    	    FLOAT,
 );
 
 CREATE TABLE ccvi (
 
-	Index                INT,
-	CommunityArea        VARCHAR(100),
-	OverallScore         FLOAT, 
-	SocioeconomicScore   FLOAT, 
-	HouseholdCrowding    FLOAT, 
-	NoVehicleHouseholds  FLOAT, 
-	PerCapitaIncome      FLOAT, 
-	Unemployment         FLOAT, 
-	NoHighSchoolDiploma  FLOAT, 
-	AgeAdjustedDeathRate FLOAT, 
-	DiabetesPrevalence   FLOAT, 
-	HIVPrevalenceRate    FLOAT, 
-	InfantMortalityRate  FLOAT, 
-	LeadPoisoningRate    FLOAT, 
-	HospitalizationRate  FLOAT
+	GeographyType        VARCHAR(100),
+	CommunityAreaOrZip   INT,
+	CommunityAreaName    VARCHAR(100), 
+	CcviCategory         VARCHAR(100), 
+	CcviScore            FLOAT, 
+	SocioeconomicStatus  INT, 
+	HouseholdComposition INT, 
+	NoPrimaryCare        INT, 
+	CumMobilityRatio     INT, 
+	FrontlineWorkers     INT, 
+	Age65OrGreater       INT, 
+	ComorbidConditions   INT, 
+	CovidIncidenceRate   INT, 
+	CovidHospitalRate    INT, 
+	CrudeMortalityRate   INT
 );
 
 CREATE TABLE censusData (
 
 	CommunityAreaNumber                   INT,
 	CommunityAreaName                     VARCHAR(100),
+	PercentOfHousingCrowded               FLOAT, 
 	PercentHouseholdsBelowPoverty         FLOAT, 
 	PercentAged16Unemployed               FLOAT, 
-	PercentAged25WithoutHighSchoolDiploma FLOAT, 
-	PercentAgedUnder18OrOver64            FLOAT, 
+	PercentAged25WithoutHighSchoolDiploma FLOAT,
+	PercentAgedUnder18OrOver64            FLOAT,
 	PerCapitaIncome                       FLOAT, 
 	HardshipIndex                         INT
 );
@@ -72,15 +69,31 @@ CREATE TABLE communityBound (
 
 CREATE TABLE covidCasesZip (
 
-    Zip        VARCHAR(100),
-    Cases      INT,
-    Tests      INT,
-    Deaths     INT,
-    PeopleTest INT
-
+	Zip          VARCHAR(100),
+	WeekNumber   INT,
+	WeekStart    VARCHAR(100),
+	WeekEnd      VARCHAR(100),
+	CasesWeekly  INT,
+	CasesMonthly INT,
+	CasesCum     INT,
+	CaseRateWkly FLOAT,
+	CaseRateMnth FLOAT,
+	CaseRateCum  FLOAT,
+	TestsWeekly  INT,
+	TestsCum     INT,
+	TestRateWkly FLOAT,
+	TestRateCum  FLOAT,
+	PosRateWkly  FLOAT,
+	PosRateCum   FLOAT,
+	DeathsWkly   INT,
+	DeathsCum    INT,
+	DeathRtWkly  FLOAT,
+	DeathRtCum   FLOAT,
+	Population   INT,
+	RowId        VARCHAR(100)
 );
 
-CREATE TABLE covidDaily (
+/*CREATE TABLE covidDaily (
 
 	Date                VARCHAR(20),
 	TotalCases          VARCHAR(20),
@@ -91,102 +104,89 @@ CREATE TABLE covidDaily (
 	NewHospitalized     VARCHAR(20),
 	AverageDailyHospitalized VARCHAR(20)
 
-);
+);*/
 
 CREATE TABLE pubHealthStats (
 
-    CommunityArea      VARCHAR(20),
-    BirthRate          VARCHAR(20),
-    GeneralFertility   VARCHAR(20),
-    LowBirthWeight     VARCHAR(20),
-    PrenatalCare       VARCHAR(20),
-    TeenBirthRate      VARCHAR(20),
-    Uninsured          VARCHAR(20),
-    BelowPoverty       VARCHAR(20),
-    CrowdedHousing     VARCHAR(20),
-    Dependency         VARCHAR(20),
-    NoDiploma          VARCHAR(20),
-    PerCapitaIncome    VARCHAR(20),
-    Unemployment       VARCHAR(20),
-    Assault            VARCHAR(20),
-    BreastCancer       VARCHAR(20),
-    Cancer             VARCHAR(20),
-    ColorectalCancer   VARCHAR(20),
-    Diabetes           VARCHAR(20),
-    FirearmMortality   VARCHAR(20),
-    InfantMortality    VARCHAR(20),
-    LungCancer         VARCHAR(20),
-    ProstateCancer     VARCHAR(20),
-    Stroke             VARCHAR(20),
-    Tuberculosis       VARCHAR(20),
-    BelowPovertyRecent VARCHAR(20),
-    NoDiplomaRecent    VARCHAR(20),
-    UnemploymentRecent VARCHAR(20)
+	CommunityArea                         VARCHAR(100),
+	CommunityAreaName                     VARCHAR(100),
+	BirthRate                             VARCHAR(100),
+	GeneralFertilityRate                  VARCHAR(100),
+	LowBirthWeight                        VARCHAR(100),
+	PrenatalCareBeginningInFirstTrimester FLOAT,
+	PretermBirths                         VARCHAR(100),
+	TeenBirthRate                         FLOAT,
+	AssaultHomicide                       VARCHAR(100),
+	BreastCancerInFemales                 FLOAT,
+	CancerAllSites                        VARCHAR(100),
+	ColorectalCancer                      FLOAT,
+	DiabetesRelated                       VARCHAR(100),
+	FirearmRelated                        FLOAT,
+	InfantMortalityRate                   FLOAT,
+	LungCancer                            FLOAT,
+	ProstateCancerInMales                 FLOAT,
+	StrokeCerebrovascularDisease          FLOAT,
+	ChildhoodBloodLeadLevelScreening      FLOAT,
+	ChildhoodLeadPoisoning                FLOAT,
+	GonorrheaInFemales                    FLOAT,
+	GonorrheaInMales                      VARCHAR(100),
+	Tuberculosis                          FLOAT, 
+	BelowPovertyLevel                     FLOAT,
+	CrowdedHousing                        FLOAT,
+	Dependency                            FLOAT,
+	NoHighSchoolDiploma                   FLOAT,
+	PerCapitaIncome                       FLOAT,
+	Unemployment                          FLOAT
 );
 
 CREATE TABLE taxiTrips (
 
-	ID                   INT,
-	TripStartTimestamp   TIMESTAMP,
-	TripEndTimestamp     TIMESTAMP,
-	TripSeconds          INT,
-	TripMiles            FLOAT, 
-	PickupCensusTract    VARCHAR(20),
-	DropoffCensusTract   VARCHAR(20),
-	PickupCommunityArea  VARCHAR(20),
-	DropoffCommunityArea VARCHAR(20),
-	Fare                 FLOAT, 
-	Tips                 FLOAT, 
-	Tolls                FLOAT, 
-	Extras               FLOAT, 
-	TripTotal            FLOAT, 
-	PaymentType          VARCHAR(20),
-	Company              VARCHAR(20),
-	TaxiID               VARCHAR(20)
+    TripID                   VARCHAR(100),
+    TaxiID                   VARCHAR(100),
+    TripStartTimestamp       TIMESTAMP,
+    TripEndTimestamp         TIMESTAMP,
+    TripSeconds              INT,
+    TripMiles                FLOAT,
+    PickupCensusTract        VARCHAR(100),
+    DropoffCensusTract       VARCHAR(100),
+    PickupCommunityArea      INT,
+    DropoffCommunityArea     INT,
+    Fare                     FLOAT,
+    Tips                     FLOAT,
+    Tolls                    FLOAT,
+    Extras                   FLOAT,
+    TripTotal                FLOAT,
+    PaymentType              VARCHAR(100),
+    Company                  VARCHAR(100),
+    PickupCentroidLatitude   FLOAT,
+    PickupCentroidLongitude  FLOAT,
+    PickupCentroidLocation   VARCHAR(100),
+    DropoffCentroidLatitude  FLOAT,
+    DropoffCentroidLongitude FLOAT,
+    DropoffCentroidLocation  VARCHAR(100)
 
 );
 
 CREATE TABLE transNetProviderTrips (
-
-	ID                         INT,
-	TripID                     VARCHAR(20),
-	PickupCommunityArea        VARCHAR(20),
-	DropoffCommunityArea       VARCHAR(20),
-	TripStartTimestamp         time.Time
-	TripEndTimestamp           time.Time
-	TripSeconds                INT,
-	TripMiles                  FLOAT, 
-	PickupCensusTract          VARCHAR(20),
-	DropoffCensusTract         VARCHAR(20),
-	PickupCentroidLatitude     FLOAT, 
-	PickupCentroidLongitude    FLOAT, 
-	DropoffCentroidLatitude    FLOAT, 
-	DropoffCentroidLongitude   FLOAT, 
-	SharedTripAuthorized       VARCHAR(20),
-	TripsPooled                INT,
-	PickupCommunityName        VARCHAR(20),
-	DropoffCommunityName       VARCHAR(20),
-	Fare                       FLOAT, 
-	Tip                        FLOAT, 
-	AdditionalCharges          FLOAT, 
-	TripTotal                  FLOAT, 
-	SharedTripPaymentType      VARCHAR(20),
-	PaymentType                VARCHAR(20),
-	Company                    VARCHAR(20),
-	TripType                   VARCHAR(20),
-	TripExtras                 FLOAT, 
-	TripStartTimeAdjusted      time.Time
-	TripEndTimeAdjusted        time.Time
-	TripMilesAdjusted          FLOAT, 
-	TripFareAdjusted           FLOAT, 
-	TripTotalAdjusted          FLOAT, 
-	SharedTripActualDistance   FLOAT, 
-	SharedTripMatchedID        VARCHAR(20),
-	OriginationLatitude        FLOAT, 
-	OriginationLongitude       FLOAT, 
-	DestinationLatitude        FLOAT, 
-	DestinationLongitude       FLOAT, 
-	SharedTripCost             FLOAT, 
-	NumberOfMatchedSharedTrips INT
-
+    TripID                   VARCHAR(100),
+    TripStartTimestamp       VARCHAR(100),
+    TripEndTimestamp         VARCHAR(100),
+    TripSeconds              INT,
+    TripMiles                FLOAT,
+    PickupCensusTract        VARCHAR(100),
+    DropoffCensusTract       VARCHAR(100),
+    PickupCommunityArea      INT,
+    DropoffCommunityArea     INT,
+    Fare                     FLOAT,
+    Tip                      FLOAT,
+    AdditionalCharges        FLOAT,
+    TripTotal                FLOAT,
+    SharedTripAuthorized     BOOLEAN,
+    TripsPooled              INT,
+    PickupCentroidLatitude   FLOAT,
+    PickupCentroidLongitude  FLOAT,
+    PickupCentroidLocation   VARCHAR(100),
+    DropoffCentroidLatitude  FLOAT,
+    DropoffCentroidLongitude FLOAT,
+    DropoffCentroidLocation  VARCHAR(100)
 );
